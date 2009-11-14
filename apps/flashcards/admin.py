c6440a26246a32102ed6842f48c19f8192e513ed
Alex Ehlke <jehlke@new-host-3.home>
2009-11-14 13:27:08 -0500

    admin adjustments

--- apps/flashcards/admin.py
+++ apps/flashcards/admin.py
@@ -1,11 +1,16 @@
 from flashcards.models import Deck, FactType, Fact, CardTemplate, FieldType, Card, FieldContent, SchedulingOptions
 from django.contrib import admin
 
+
+class CardAdmin(admn.ModelAdmin):
+    exclude = ('synchronized_with',)
+
+
 admin.site.register(Deck)
 admin.site.register(FactType)
 admin.site.register(Fact)
 admin.site.register(CardTemplate)
 admin.site.register(FieldType)
-admin.site.register(Card)
+admin.site.register(Card, CardAdmin)
 admin.site.register(FieldContent)
 admin.site.register(SchedulingOptions)
