2dc54b407b1e4bcca2b5826b7f60af4df0085d74
Alex Ehlke <jehlke@Alex-Ehlkes-MacBook-Pro.local>
2009-12-06 02:17:54 -0500

    updating deck pages

--- apps/utils/templatetags/xhrlinks.py
+++ apps/utils/templatetags/xhrlinks.py
@@ -7,3 +7,6 @@ register = template.Library()
 def xhrlink(url, text):
     return u'<a href="{url}" onclick="manabi_ui.xhrLink(this.href);return false;">{text}</a>'.format(url=url, text=text)
 
+@register.simple_tag
+def xhrlink_onclick():
+    return u'onclick="manabi_ui.xhrLink(this.href);return false;"'
