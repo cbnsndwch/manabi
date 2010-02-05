4ccf3170a4c1c952a053d22827140934568bca08
Alex Ehlke <jehlke@student.umass.edu>
2010-02-05 00:07:41 -0500

    fixing timezone default

--- forms.py
+++ forms.py
@@ -3,7 +3,7 @@ from timezones.forms import TimeZoneField
 from django.utils.translation import ugettext_lazy as _, ugettext
 
 class SignupForm(PinaxSignupForm):
-    timezone = TimeZoneField(label=_("Timezone"), required=True, initial='America/New-York')
+    timezone = TimeZoneField(label=_("Timezone"), required=True, initial='America/New_York')
 
 class OpenIDSignupForm(PinaxOpenIDSignupForm):
-    timezone = TimeZoneField(label=_("Timezone"), required=True, initial='America/New-York')
+    timezone = TimeZoneField(label=_("Timezone"), required=True, initial='America/New_York')
