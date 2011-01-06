d29c6ff944d2f372ecb6f58c21ef1f6917be761c
Alex Ehlke <captlou@gmail.com>
2011-01-06 07:08:56 -0500

    added active nav tab detection and handling

---
+++ apps/utils/templatetags/activetab_tags.py
@@ -0,0 +1,35 @@
+from django import template
+from snippetscream import resolve_to_name
+from manabi import tabs
+
+register = template.Library()
+
+@register.simple_tag
+def is_active_tab(request, tab_name):
+    '''
+    Returns 'active' if this tab is the active one.
+    Otherwise returns ''.
+    '''
+
+    url_name = resolve_to_name(request.path)
+
+    if url_name in tabs.TAB_URLS[tab_name]:
+        return 'active'
+    else:
+        return ''
+
+@register.simple_tag
+def active_tab(request):
+    url_name = resolve_to_name(request.path)
+    print request.path
+    print url_name
+
+    for tab_name, urls in tabs.TAB_URLS.iteritems():
+        if url_name in urls:
+            return tab_name
+
+    return ''
+
+
+
+
---
+++ tabs.py
@@ -0,0 +1,19 @@
+
+# Define which URL names fall under which tabs on the site
+
+TAB_URLS = {
+    'home': [
+        'home',
+        'home_inline',
+    ],
+    'add': [
+        'add_decks',
+        'create_deck',
+    ],
+    'manage': [
+        'decks',
+        'update_deck',
+        'delete_deck',
+    ],
+}
+
