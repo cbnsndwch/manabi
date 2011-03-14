fa526ed9ff4633f3d02bb310e627e2e089131297
Alex Ehlke <alex.ehlke@gmail.com>
2011-03-14 03:33:22 -0400

    django csrf security update fixes, and some REST api stuff

--- apps/flashcards/views/shortcuts.py
+++ apps/flashcards/views/shortcuts.py
@@ -10,13 +10,19 @@ from django.shortcuts import get_object_or_404
 def get_deck_or_404(user, pk):
     '''
     Returns the deck with the given pk. 404s, or raises an exception
-    if the user doesn't own that deck.
+    if the user doesn't own that deck and it's not shared.
     '''
     deck = get_object_or_404(Deck, pk=pk)
-    if deck.owner_id != user.id:
-        #TODO should be a permissions error instead
-        raise forms.ValidationError(
-            'You do not have permission to access this flashcard deck.')
+
+    # The user must either own it, or it must be a shared deck.
+    if deck.owner != user and not deck.shared:
+        raise HttpForbiddenException(
+                'You do not have permission to view this deck. '
+                'This deck is not shared.')
+    #if deck.owner_id != user.id:
+    #    #TODO should be a permissions error instead
+    #    raise forms.ValidationError(
+    #        'You do not have permission to access this flashcard deck.')
     return deck
 
 
