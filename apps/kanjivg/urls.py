3ecf6282570c582d823222a3bd358afa43d59a0a
Alex Ehlke <alex.ehlke@gmail.com>
2011-04-08 02:43:37 -0400

    splinter kanjivg app into its own project, to lighten this one

--- apps/kanjivg/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls.defaults import *
-
-# place app url patterns here
-urlpatterns = patterns('kanjivg.views',
-    url(r'^frames/(?P<ordinal>\d+).json$',
-        'frames_json',
-        name='kanjivg_frames_json'),
-)
