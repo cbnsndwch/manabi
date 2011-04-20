e756c22e42886c3818e21ba8077b4ddad9c81c66
Alex Ehlke <alex.ehlke@gmail.com>
2011-04-20 05:33:43 -0400

    fix slugification

--- apps/utils/slugs.py
+++ apps/utils/slugs.py
@@ -13,7 +13,7 @@ def slugify(text, delim=u'-', max_length=50):
 
     result = []
     for word in _punct_re.split(text.lower()):
-        new_result = result + [unidecode(word).split()]
+        new_result = result + unidecode(word).split()
         if len(join(new_result)) <= max_length:
             result = new_result
         else:
