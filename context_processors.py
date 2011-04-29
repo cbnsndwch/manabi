dd998bbf6035883707bee6e4833115558b0cb6c1
Alex Ehlke <alex.ehlke@gmail.com>
2011-04-29 03:17:50 -0400

    let googlebot see full pages for ajax fragments

--- context_processors.py
+++ context_processors.py
@@ -1,11 +1,25 @@
 
 def site_base_extender(request):
     '''
-    adds `base_template_name` to context
+    Adds `base_template_name` to context for pages like the ToS.
+
+    And `fragment_base_template_name` for ajax pages that need to be rendered
+    differently for googlebot.
     '''
-    return {
-        'base_template_name': (
-            'body_pane_base.html' if request.is_ajax()
-            else 'site_base.html'
-    )}
+    ctx = {}
+
+    if request.is_ajax():
+        name = 'body_pane_base.html' 
+    else:
+        name ='site_base.html'
+    ctx['base_template_name'] = name
+
+    # Googlebot?
+    if request.META.get('HTTP_X_ESCAPED_FRAGMENT', 'false').lower() == 'true':
+        fragment_name = 'site_base.html'
+    else:
+        fragment_name = 'body_pane_base.html'
+    ctx['fragment_base_template_name'] = fragment_name
+
+    return ctx
 
