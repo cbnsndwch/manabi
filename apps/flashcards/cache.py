a9f7d0b17714c91b57eb1c92594c1469aa1157ef
Alex Ehlke <alex.ehlke@gmail.com>
2011-05-09 01:46:27 -0400

    pk is the pk more reliably than id is.

--- apps/flashcards/cache.py
+++ apps/flashcards/cache.py
@@ -7,14 +7,18 @@ from cachecow.cache import invalidate_namespace
 @receiver(fact_grid_updated, dispatch_uid='nuke_fact_grid_namespace')
 def nuke_fact_grid_namespace(sender, decks=[], **kwargs):
     for deck in decks:
-        invalidate_namespace(fact_grid_namespace(deck=deck.id))
+        invalidate_namespace(fact_grid_namespace(deck=deck.pk))
  
 
 def fact_grid_namespace(deck=None, *args, **kwargs):
     '''
-    This namespace is keyed on the deck ID.
+    This namespace is keyed on the deck PK.
     '''
     print 'fact_grid_namespace'
     return ['fact_grid', deck]
 
+###############################################################################
 
+#@receiver(post_save, sender=FieldContent, dispatch_uid='human_readable_field_ps')
+#def nuke_human_readable_field(sender, instance, created, **kwargs):
+    
