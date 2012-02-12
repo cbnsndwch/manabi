29242d4afd448b4c2925bf9f392b2f5a2de84548
Alex Ehlke <alex.ehlke@gmail.com>
2012-02-12 16:20:04 -0500

    gunicorn logfile

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -2,6 +2,7 @@ import multiprocessing
 
 bind = '127.0.0.1:3031'
 workers = multiprocessing.cpu_count() * 2 + 1
-log_file = '/var/log/gunicorn-manabi.log'
+#log_file = '/var/log/gunicorn-manabi.log'
+logfile = '/var/log/manabi/gunicorn.log'
 pid = '/tmp/cherokee-gunicorn-manabi.pid'
 
