fa6b942f385a534771759eeb203f288b6b40ca38
Alex Ehlke <alex.ehlke@gmail.com>
2012-02-12 16:27:50 -0500

    pidfile

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -4,5 +4,5 @@ bind = '127.0.0.1:3031'
 workers = multiprocessing.cpu_count() * 2 + 1
 #log_file = '/var/log/gunicorn-manabi.log'
 errorlog = '/var/log/manabi/gunicorn.error.log'
-pidfile = '/tmp/gunicorn-manabi.pid'
+pidfile = '/tmp/manabi-gunicorn.pid'
 
