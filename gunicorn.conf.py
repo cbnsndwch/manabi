3faffadc3e77ba4ea6711feb6838fabe8e0e53f2
Alex Ehlke <alex.ehlke@gmail.com>
2012-03-20 00:05:31 -0400

    tweak gunicorn

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,7 +1,7 @@
 import multiprocessing
 
 bind = '127.0.0.1:3031'
-workers = multiprocessing.cpu_count() * 2 + 1
+workers = multiprocessing.cpu_count() * 2
 #log_file = '/var/log/gunicorn-manabi.log'
 errorlog = '/var/log/manabi/gunicorn.error.log'
 pidfile = '/tmp/manabi-gunicorn.pid'
