6b485fddae4522fc37d029b8eacebebb118d8577
Alex Ehlke <alex.ehlke@gmail.com>
2015-12-31 00:22:36 -0500

    add suspended to deck resources

--- manabi/apps/flashcards/restresources.py
+++ manabi/apps/flashcards/restresources.py
@@ -14,17 +14,18 @@ class DeckResource(RestModelResource):
         'shared_at',
         'created_at',
         'modified_at',
+        'suspended',
     ]
 
     def get_url_path(self):
-        return ''#TODO reverse('rest-deck', args=[self.obj.id])
+        return '' #TODO reverse('rest-deck', args=[self.obj.id])
 
     def get_data(self):
         data = super(DeckResource, self).get_data()
         data.update({
             'owner': UserResource(self.obj.owner).get_data(),
             'card_count': self.obj.card_count(),
-            'status_url': '',#TODO reverse('rest-deck_status', args=[self.obj.id]),
+            'status_url': '', #TODO reverse('rest-deck_status', args=[self.obj.id]),
         })
         return data
 
