8e42957833c52489a10e85bdc895d18d4d9c3874
Alex Ehlke <alex.ehlke@gmail.com>
2016-10-10 18:30:15 -0400

    configure raven for prod

--- manabi/settings_production.py
+++ manabi/settings_production.py
@@ -1,3 +1,13 @@
+import os
+import raven
+
+RAVEN_CONFIG = {
+    'dsn': 'https://d074e80bd2b349118459c80495184cfd:5a9e697b456241d587fd62e27f8b6faa@sentry.io/105057',
+    # If you are using git, you can also automatically configure the
+    # release based on the git info.
+    'release': raven.fetch_git_sha(os.path.dirname(__file__)),
+}
+
 REDIS = {
     'host': 'localhost',
     'port': 6378,
