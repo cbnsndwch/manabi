7302d4287e87559f74a448d9f7caf1bca29e0386
Alex Ehlke <alex.ehlke@gmail.com>
2016-10-12 21:53:31 -0400

    fix wsgi configuration

--- manabi/wsgi.py
+++ manabi/wsgi.py
@@ -1,12 +1,17 @@
+'''
+WSGI config for Manabi.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
+'''
+
 import os
 
+from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'manabi.settings')
-
-# This application object is used by the development server
-# as well as any WSGI server configured to use this file.
-from django.core.wsgi import get_wsgi_application
 
 application = Cling(get_wsgi_application())
