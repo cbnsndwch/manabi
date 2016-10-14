540e5b4d36659d3d07ca51bd51f5932e1e126f01
Alex Ehlke <alex.ehlke@gmail.com>
2016-10-13 22:32:53 -0400

    disable existing loggers

--- manabi/settings.py
+++ manabi/settings.py
@@ -66,6 +66,7 @@ if DEBUG:
 else:
     LOGGING = {
         'version': 1,
+        'disable_existing_loggers': True,
         'formatters': {
             'verbose': {
                 'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s',
