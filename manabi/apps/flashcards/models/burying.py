413b2607b1cd55384bba1dddc0f067d50b750c29
Alex Ehlke <alex.ehlke@gmail.com>
2016-10-16 23:45:05 -0400

    make order_by optional

--- manabi/apps/flashcards/models/burying.py
+++ manabi/apps/flashcards/models/burying.py
@@ -1,11 +1,19 @@
-def with_siblings_buried(cards, order_by):
+def with_siblings_buried(cards, order_by=None):
     '''
     Removes siblings from a queryset.
     '''
     from manabi.apps.flashcards.models import Card
 
-    cards_with_burying_applied = (
-        cards.order_by('fact_id', order_by).distinct('fact_id'))
-    return Card.objects.filter(
-        pk__in=cards_with_burying_applied,
-    ).order_by(order_by)
+    if order_by is None:
+        ordered_cards = cards.order_by('fact_id')
+    else:
+        ordered_cards = cards.order_by('fact_id', order_by)
+
+    cards_with_burying_applied = ordered_cards.distinct('fact_id')
+
+    if order_by is None:
+        return cards_with_burying_applied
+    else:
+        return Card.objects.filter(
+            pk__in=cards_with_burying_applied,
+        ).order_by(order_by)
