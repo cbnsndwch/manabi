9d360def110b369819547b33e726617462964618
Alex Ehlke <alex.ehlke@gmail.com>
2016-10-16 23:55:32 -0400

    to be safe...

--- manabi/settings.py
+++ manabi/settings.py
@@ -31,6 +31,7 @@ MANAGERS = ADMINS
 if DEBUG:
     LOGGING = {
         'version': 1,
+        'disable_existing_loggers': True,
         'formatters': {
             'verbose': {
                 'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s',
